2e9335ee40ec25ef019757f21a83a066b8bccf66
Stefan Netterfield <stefan@5f03.com>
2023-07-07 07:45:43 -0400

    Added additional configs for project...

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,18 @@
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import node from '@astrojs/node';
+import image from '@astrojs/image';
+import glsl from 'vite-plugin-glsl';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: 'server',
+  integrations: [tailwind(), react(), image()],
+  adapter: node({
+    mode: 'standalone'
+  }),
+  vite: {
+    plugins: [glsl()]
+  }
+});
