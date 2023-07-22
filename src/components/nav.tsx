dfcd1a1e4c5f76aa74cc7c9f95d154a86706ae7b
Stefan Netterfield <stefan@5f03.com>
2023-07-21 21:27:32 -0400

    Refactored nav links to use dark red for hover color...

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -23,7 +23,7 @@ export function NavBar({children}: PropsWithChildren): ReactElement<FC> {
 					<img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
 				</a>
 				<div className="flex md:hidden">
-					<button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5" onClick={handleClick}>
+					<button type="button" className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 transition-colors ease-linear hover:text-dark-red" onClick={handleClick}>
 						<span className="sr-only">Open main menu</span>
 						<FontAwesomeIcon icon={faBars} className="h-6 w-6" aria-hidden="true" />
 					</button>
@@ -49,13 +49,13 @@ export function PrimaryOffCanvasMenu(): ReactElement<FC> {
                         leave="transition transform transform-gpu duration-400"
                         leaveFrom="opacity-100 scale-100"
                         leaveTo="opacity-0 scale-50">
-					<Dialog.Panel className="fixed inset-0 z-10 w-full overflow-y-auto px-6 py-6 bg-neutral-800 text-neutral-400">
+					<Dialog.Panel className="fixed inset-0 z-10 w-full overflow-y-auto px-6 py-6 bg-neutral-800 text-neutral-200">
 						<div className="flex items-center justify-between">
 							<a href="#" className="-m-1.5 p-1.5">
 								<span className="sr-only">Your Company</span>
 								<img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
 							</a>
-							<button type="button" className="-m-2.5 rounded-md p-2.5 transition-colors ease-linear hover:text-neutral-200" onClick={handleClick}>
+							<button type="button" className="-m-2.5 rounded-md p-2.5 transition-colors ease-linear hover:text-dark-red" onClick={handleClick}>
 								<span className="sr-only">Close menu</span>
 								<FontAwesomeIcon icon={faXmark} className="h-6 w-6" aria-hidden="true" />
 							</button>
@@ -65,32 +65,32 @@ export function PrimaryOffCanvasMenu(): ReactElement<FC> {
 								<ul className="space-y-2 py-6">
                                     <li
 									    key="home"
-										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-neutral-200">
+										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-dark-red">
 										<a href="#">Home</a>
 									</li>
                                     <li
 									    key="about"
-										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-neutral-200">
+										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-dark-red">
 										<a href="#">About</a>
 									</li>
                                     <li
 									    key="skills"
-										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-neutral-200">
+										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-dark-red">
 										<a href="#">Skills</a>
 									</li>
                                     <li
 									    key="projects"
-										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-neutral-200">
+										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-dark-red">
 										<a href="#">Projects</a>
 									</li>
                                     <li
 									    key="contact"
-										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-neutral-200">
+										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-dark-red">
 										<a href="#">Contact</a>
 									</li>
                                     <li
 									    key="blog"
-										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-neutral-200">
+										className="-mx-3 block transition-colors ease-linear rounded-lg px-3 py-2 text-2xl text-center font-bold leading-7 hover:text-dark-red">
 										<a href="#">Blog</a>
 									</li>
 								</ul>
