2b611ba2d74e6ddb5db4db825ab1b88385658bd3
Stefan Netterfield <stefan@5f03.com>
2023-07-30 18:21:05 -0400

    Fixed linkedin url for linkedin badge...

--- src/components/layout/socialIcons.tsx
+++ src/components/layout/socialIcons.tsx
@@ -6,7 +6,7 @@ export default function SocialIcons(): ReactElement<FC> {
     return (
         <ul className="flex flex-row justify-center md:justify-end">
             <li className="px-1 md:px-3">
-                <a target="_blank" href="www.linkedin.com/in/stefan-netterfield-981b09269">
+                <a target="_blank" href="https://www.linkedin.com/in/stefan-netterfield-981b09269">
                     <FontAwesomeIcon icon={faLinkedin} className="w-7 h-7 transition-colors ease-linear hover:text-dark-red" />
                 </a>
             </li>
