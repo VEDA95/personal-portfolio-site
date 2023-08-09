b7443ee0b3567d0136708da8ffd64c24be99cdb2
Stefan Netterfield <stefan@5f03.com>
2023-08-09 04:47:50 -0400

    Added state and event handlers to contact form...

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,19 +1,43 @@
 import { useState, useCallback } from 'react';
 import classNames from 'classnames';
-import type { ReactElement, FC, FormEvent } from 'react';
+import type { ReactElement, FC, FormEvent, SyntheticEvent } from 'react';
 
 export interface ContactFormProps {
 	widthClass?: string;
 }
 
+interface IFormTextState {
+	'first-name': string;
+	'last-name': string;
+	'email': string;
+	'company': string;
+	'budget': string;
+	'message': string;
+}
+
 export default function ContactForm({ widthClass }: ContactFormProps): ReactElement<FC> {
+	const [formData, setFormData] = useState<IFormTextState>({'first-name': '', 'last-name': '', email: '', company: '', budget: '', message: ''});
+	const [formServicesData, setFormServicesData] = useState<Array<string>>([]);
 	const formContainerClasses: string = classNames('flex flex-col justify-center min-h-[calc(100vh_-_7rem)]', {
 		'w-full': widthClass == null || widthClass?.length === 0,
 		[widthClass as string]: widthClass != null && widthClass.length > 0
 	});
+	const handleTextChange = useCallback((e: SyntheticEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>): void => {
+		const {currentTarget: {name, value}} = e;
+		setFormData((state: IFormTextState): IFormTextState => ({...state, [name]: value}));
+	}, []);
+	const handleServicesChange = useCallback((e: SyntheticEvent<HTMLInputElement>): void => {
+		const {currentTarget: {checked, name}} = e;
+		setFormServicesData((state: Array<string>): Array<string> => {
+			console.log(state);
+			if(!checked) return state.filter((elem: string): boolean => elem !== name);
+
+			return [...state, name];
+		});
+	}, []);
 	const handleSubmit = useCallback((e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
-	}, []);
+	}, [formData, formServicesData]);
 
 	return (
 		<div className={formContainerClasses}>
@@ -28,6 +52,8 @@ export default function ContactForm({ widthClass }: ContactFormProps): ReactElem
 								type="text"
 								name="first-name"
 								id="first-name"
+								value={formData['first-name']}
+								onChange={handleTextChange}
 								className="block w-full rounded-md border-0 px-3.5 py-2 bg-neutral-800 placeholder:text-neutral-400 shadow-sm ring-1 ring-inset ring-neutral-400 focus:ring-2 focus:ring-dark-red sm:text-sm sm:leading-6"
 							/>
 						</div>
@@ -41,6 +67,8 @@ export default function ContactForm({ widthClass }: ContactFormProps): ReactElem
 								type="text"
 								name="last-name"
 								id="last-name"
+								value={formData['last-name']}
+								onChange={handleTextChange}
 								autoComplete="family-name"
 								className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset bg-neutral-800 ring-neutral-400 placeholder:text-neutral-400 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dark-red sm:text-sm sm:leading-6"
 							/>
@@ -56,6 +84,8 @@ export default function ContactForm({ widthClass }: ContactFormProps): ReactElem
 								name="email"
 								id="email"
 								autoComplete="email"
+								value={formData.email}
+								onChange={handleTextChange}
 								className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset bg-neutral-800 ring-neutral-400 placeholder:text-neutral-400 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dark-red sm:text-sm sm:leading-6"
 							/>
 						</div>
@@ -71,6 +101,8 @@ export default function ContactForm({ widthClass }: ContactFormProps): ReactElem
 								name="company"
 								id="company"
 								autoComplete="organization"
+								value={formData.company}
+								onChange={handleTextChange}
 								className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset bg-neutral-800 ring-neutral-400 placeholder:text-neutral-400 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dark-red sm:text-sm sm:leading-6"
 							/>
 						</div>
@@ -84,6 +116,8 @@ export default function ContactForm({ widthClass }: ContactFormProps): ReactElem
 								id="budget"
 								name="budget"
 								autoComplete="budget"
+								value={formData.budget}
+								onChange={handleTextChange}
 								className="block w-full rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset bg-neutral-800 ring-neutral-400 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dark-red sm:text-sm sm:leading-6"
 							>
 								<option>$500 - $1,000</option>
@@ -98,15 +132,27 @@ export default function ContactForm({ widthClass }: ContactFormProps): ReactElem
 						<legend className="block text-sm font-semibold leading-6">Services</legend>
                         <fieldset id="services" className="flex flex-row w-full flex-wrap gap-2 pt-2">
                                 <div className="flex flex-col">
-                                    <input id="web_development" name="services" type="checkbox" className="peer hidden"/>
+                                    <input
+										id="web-development"
+										name="web-development"
+										type="checkbox"
+										className="peer hidden"
+										checked={formServicesData.includes('web-development')}
+										onChange={handleServicesChange} />
                                     <label
-                                        htmlFor="web_development"
+                                        htmlFor="web-development"
                                         className="inline-flex items-center gap-x-1.5 rounded-full px-2 py-1 text-xs font-medium ring-1 ring-inset ring-neutral-400 select-none cursor-pointer transition-colors duration-300 ease-in-out peer-hover:bg-dark-red peer-checked:bg-dark-red peer-checked:border-neutral-200">
                                             Web Development
                                     </label>
                                 </div>
                                 <div className="flex flex-col">
-                                    <input id="web_design" name="services" type="checkbox" className="peer hidden"/>
+                                    <input
+										id="web_design"
+										name="web-design"
+										type="checkbox"
+										className="peer hidden"
+										checked={formServicesData.includes('web-design')}
+										onChange={handleServicesChange} />
                                     <label
                                         htmlFor="web_design"
                                         className="inline-flex items-center gap-x-1.5 rounded-full px-2 py-1 text-xs font-medium ring-1 ring-inset ring-neutral-400 select-none cursor-pointer transition-colors duration-300 ease-in-out peer-hover:bg-dark-red peer-checked:bg-dark-red peer-checked:border-neutral-200">
@@ -114,7 +160,13 @@ export default function ContactForm({ widthClass }: ContactFormProps): ReactElem
                                     </label>
                                 </div>
                                 <div className="flex flex-col">
-                                    <input id="sys_administration" name="services" type="checkbox" className="peer hidden"/>
+                                    <input
+										id="sys_administration"
+										name="sys-administration"
+										type="checkbox"
+										className="peer hidden"
+										checked={formServicesData.includes('sys-administration')} 
+										onChange={handleServicesChange} />
                                     <label
                                         htmlFor="sys_administration"
                                         className="inline-flex items-center gap-x-1.5 rounded-full px-2 py-1 text-xs font-medium ring-1 ring-inset ring-neutral-400 select-none cursor-pointer transition-colors duration-300 ease-in-out peer-hover:bg-dark-red peer-checked:bg-dark-red peer-checked:border-neutral-200">
@@ -122,7 +174,13 @@ export default function ContactForm({ widthClass }: ContactFormProps): ReactElem
                                     </label>
                                 </div>
                                 <div className="flex flex-col">
-                                    <input id="network_administration" name="services" type="checkbox" className="peer hidden"/>
+                                    <input
+										id="network_administration"
+										name="network-administration"
+										type="checkbox"
+										className="peer hidden"
+										checked={formServicesData.includes('network-administration')} 
+										onChange={handleServicesChange} />
                                     <label
                                         htmlFor="network_administration"
                                         className="inline-flex items-center gap-x-1.5 rounded-full px-2 py-1 text-xs font-medium ring-1 ring-inset ring-neutral-400 select-none cursor-pointer transition-colors duration-300 ease-in-out peer-hover:bg-dark-red peer-checked:bg-dark-red peer-checked:border-neutral-200">
@@ -130,7 +188,13 @@ export default function ContactForm({ widthClass }: ContactFormProps): ReactElem
                                     </label>
                                 </div>
                                 <div className="flex flex-col">
-                                    <input id="something_else" name="services" type="checkbox" className="peer hidden"/>
+                                    <input
+										id="something_else"
+										name="something-else"
+										type="checkbox"
+										className="peer hidden"
+										checked={formServicesData.includes('something-else')}
+										onChange={handleServicesChange} />
                                     <label
                                         htmlFor="something_else"
                                         className="inline-flex items-center gap-x-1.5 rounded-full px-2 py-1 text-xs font-medium ring-1 ring-inset ring-neutral-400 select-none cursor-pointer transition-colors duration-300 ease-in-out peer-hover:bg-dark-red peer-checked:bg-dark-red peer-checked:border-neutral-200">
@@ -149,8 +213,8 @@ export default function ContactForm({ widthClass }: ContactFormProps): ReactElem
 								id="message"
 								rows={4}
 								className="block w-full resize-none overflow-y-auto overflow-x-hidden h-52 rounded-md border-0 px-3.5 py-2 shadow-sm ring-1 ring-inset bg-neutral-800 ring-neutral-400 placeholder:text-neutral-400 focus-within:ring-2 focus-within:ring-inset focus-within:ring-dark-red sm:text-sm sm:leading-6"
-								defaultValue={''}
-							/>
+								value={formData.message}
+								onChange={handleTextChange} />
 						</div>
 					</div>
 				</div>
