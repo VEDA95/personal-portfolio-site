260a8f59c036fc40ccb4d80357ea6439c4e83c9a
Stefan Netterfield <stefan@5f03.com>
2023-09-24 00:26:56 -0400

    Created post panels for blog...

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -1,22 +1,19 @@
 import client from './directus';
 import { readItem, readItems } from '@directus/sdk';
 
+const blogFields: Array<string> = ['*', 'postimg.*'];
 
 export async function fetchBlogPosts(): Promise<any> {
-    return await client.request(readItems('posts', {
-        fields: ['*']
-    }));
+    return await client.request(readItems('posts', {fields: blogFields}));
 }
 
 export async function fetchBlogPost(id: string): Promise<any> {
-    return await client.request(readItem('posts', id, {
-        fields: ['*']
-    }));
+    return await client.request(readItem('posts', id, {fields: blogFields}));
 }
 
 export async function fetchBlogPostsByCategory(category: string): Promise<any> {
     return await client.request(readItems('posts', {
-        fields: ['*'],
+        fields: blogFields,
         filter: {
             category: {
                 _eq: category
