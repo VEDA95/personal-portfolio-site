0081f4f3464fb4b1a8666958f1184af8cc160dbf
Stefan Netterfield <stefan@5f03.com>
2023-09-25 05:31:03 -0400

    Refactor blog post fetch functions to included a filter for published posts...

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -5,17 +5,34 @@ import type { Posts, Post } from './types/blog';
 const blogFields: Array<string> = ['*', 'postimg.*'];
 
 export async function fetchBlogPosts(): Promise<Posts> {
-    return await client.request(readItems('posts', {fields: blogFields}));
+    return await client.request(readItems('posts', {
+        fields: blogFields,
+        filter: {
+            status: {
+                _eq: 'published'
+            }
+        }
+    }));
 }
 
 export async function fetchBlogPost(id: string): Promise<Post> {
-    return await client.request(readItem('posts', id, {fields: blogFields}));
+    return await client.request(readItem('posts', id, {
+        fields: blogFields,
+        filter: {
+            status: {
+                _eq: 'published'
+            }
+        }
+    }));
 }
 
 export async function fetchBlogPostsByCategory(category: string): Promise<Posts> {
     return await client.request(readItems('posts', {
         fields: blogFields,
         filter: {
+            status: {
+                _eq: 'published'
+            },
             category: {
                 _eq: category
             }
