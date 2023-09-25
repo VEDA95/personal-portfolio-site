cb9a025c28068b6124c2927b24797b636da63356
Stefan Netterfield <stefan@5f03.com>
2023-09-25 05:16:40 -0400

    Added types for blog post data from backend...

--- src/data/blog.ts
+++ src/data/blog.ts
@@ -1,17 +1,18 @@
 import client from './directus';
 import { readItem, readItems } from '@directus/sdk';
+import type { Posts, Post } from './types/blog';
 
 const blogFields: Array<string> = ['*', 'postimg.*'];
 
-export async function fetchBlogPosts(): Promise<any> {
+export async function fetchBlogPosts(): Promise<Posts> {
     return await client.request(readItems('posts', {fields: blogFields}));
 }
 
-export async function fetchBlogPost(id: string): Promise<any> {
+export async function fetchBlogPost(id: string): Promise<Post> {
     return await client.request(readItem('posts', id, {fields: blogFields}));
 }
 
-export async function fetchBlogPostsByCategory(category: string): Promise<any> {
+export async function fetchBlogPostsByCategory(category: string): Promise<Posts> {
     return await client.request(readItems('posts', {
         fields: blogFields,
         filter: {
